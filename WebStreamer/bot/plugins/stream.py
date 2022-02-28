c00c5964736eb18d443ef2a8d5c7dcd3ba04c980
dilla <dillahapsarii@gmail.com>
2022-02-28 05:59:39 +0000

    fdg

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -31,10 +31,25 @@ async def media_receive_handler(_, m: Message):
     stream_link = f"{Var.URL}{log_msg.message_id}/{quote_plus(get_name(m))}?hash={get_hash(log_msg)}"
     short_link = f"{Var.URL}{get_hash(log_msg)}{log_msg.message_id}"
     logging.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
+    await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {short_link}", disable_web_page_preview=True, parse_mode="Markdown", quote=True)
+    
+    file_name = None
+    if m.video:
+        file_name = f"{m.video.file_name}"
+    elif m.document:
+        file_name = f"{m.document.file_name}"
+    elif m.audio:
+        file_name = f"{m.audio.file_name}"
+    msg_text ="""
+<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>
+
+<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>
+
+<b>📺 LINK  :</b> <i>{}</i>
+
+<b>🚸 Nᴏᴛᴇ : LINK WON'T EXPIRE TILL DELETED</b>"""
     await m.reply_text(
-        text="<code>{}</code>\n(<a href='{}'>shortened</a>)".format(
-            stream_link, short_link
-        ),
+        text=msg_text.format(file_name, short_link),
         quote=True,
         parse_mode="html",
         reply_markup=InlineKeyboardMarkup(
